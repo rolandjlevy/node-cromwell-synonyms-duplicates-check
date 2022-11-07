615883f1a1942db8675379e2c5999ecf8a8f026f
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2022-11-07 12:33:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { synonyms } = require('./synonyms');
+const { keywords } = require('./keywords');
+
+const duplicates = keywords.filter(item => {
+  const found = synonyms
+    .filter(n => n
+      .split(',')
+      .find((str, index) => index === 0 && str === item));
+  return found.length && found;
+});
+
+console.log(duplicates.join('\n'));
